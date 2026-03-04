3cc503cb81109c1a64ea213710fc2ece27e733d0
onlyjaideep <frontenddevjai@gmail.com>
2026-03-04 13:13:34 +0530

    even/odd task

---
+++ Assignment_03.py
@@ -0,0 +1,6 @@
+#task- check if the integer is even or odd
+num = int(input("Enter an integer: "))
+if num % 2 == 0:
+    print(num, "is an even number.")
+else:
+    print(num, "is an odd number.")
---
+++ Assignment_04.py
@@ -0,0 +1,5 @@
+#task-add numbers upto 50 (1-50)
+Total = 0
+for i in range(1,51):
+    Total = Total + i
+print("The sum of numbers from 1 to 50 is:", Total)
